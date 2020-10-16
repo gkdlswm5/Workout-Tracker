63fb91bb831c09d058e379d4c3fe538d23e149ec
gkdlswm5 <67978867+gkdlswm5@users.noreply.github.com>
2020-10-15 21:04:01 -0700

    added get

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 // const routes = require("./routes")
 const path = require("path");
-const db = require("./models")
+const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
-
 
 const app = express();
 
@@ -19,54 +18,67 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-mongoose.connection.once('open', function() {
-  console.log("Database connected")
-})
-
-//html
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "./public/index.html"))
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
 });
 
-app.get("/exercise", function(req, res){
-  res.sendFile(path.join(__dirname, "./public/exercise.html"))
+mongoose.connection.once("open", function () {
+  console.log("Database connected");
 });
 
-app.get("/stats", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/stats.html"))
-})
+//HTML
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
+app.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
 
 //POST route
 app.post("/api/workouts", (req, res) => {
   console.log(req.body);
-  db.Workout.create(req.body)
-  .then((workout) => res.json(workout));;
+  db.Workout.create(req.body).then((workout) => res.json(workout));
 });
 
 //GET route
 app.get("/api/workouts", (req, res) => {
-  db.Workout.find({}).then((data) => res.json(data))
+  db.Workout.find({})
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      console.log(`getWorkoutsInRange: ${dbWorkout}`);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
+//PUT route
 app.put("/api/workouts/:id", (req, res) => {
   console.log(req.body);
-  db.Workout.findByIdAndUpdate(
-    req.params.id, {
-      $push : 
-      {
-        excercise: req.body
-      }
-    }
-  ).then(data => {
-    res.json(data);
-    console.log(data)
-  }).catch(err => console.log(err))
+  db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      excercise: req.body,
+    },
+  })
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
 });
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
